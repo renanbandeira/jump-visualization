b79b8dc25e8b7692ee767f0ddc4495bd36bc1bed
Renan Bandeira <renanbandeira2@gmail.com>
2018-02-15 06:50:51 -0300

    Uses only usable data

--- src/App.js
+++ src/App.js
@@ -13,12 +13,15 @@ firebase.initializeApp(firebaseConfig);
 class App extends Component {
   render() {
     const { events } = this.props;
+    // 25-11: Salto com sensor - algoritmo de aprendizagem não convergiu
+    // 06-01: Salto com acelerometro do dispositivo - algoritmo convergiu com 100%
+    // 23-01: Novos saltos com acelerometro do dispositivo - validação ficou em 66%
+    const datesOfJumps = ['25-11-17', '06-01-18', '23-01-18'];
     const jumps = [];
     if (events) {
       let immutableEvents = fromJS(events);
-      const filter = immutableEvents.keySeq().sort().last().substring(0,8);
       immutableEvents = immutableEvents.filter((value, key) =>
-      key.indexOf(filter) !== -1 && (value.get('type') === 0 || value.get('type') === 1));
+      datesOfJumps.indexOf(key.substring(0, 8)) !== -1 && (value.get('type') === 0 || value.get('type') === 1)).sort();
       if (immutableEvents.size === 0 ) {
         jumps.push(<p key="fetching">No results found</p>);
       }
